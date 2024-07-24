b12eb71bfcd127ca19b385a7d1f45f89e4d76ef2
wuzbre <15575079135@163.com>
2024-07-24 20:34:07 +0800

    msg:快慢指针，需要通过判定快慢指针在什么条件下需要移动

--- src/main/java/com/wuzbre/array/RemoveElement.java
+++ src/main/java/com/wuzbre/array/RemoveElement.java
@@ -1,8 +1,27 @@
 package com.wuzbre.array;
 
 public class RemoveElement {
+    /**
+     * 移除元素使用快慢指针
+     * @param nums
+     * @param target
+     * @return
+     */
+
+    public static int removeElement(int[] nums, int target) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != target){
+                nums[slowIndex]=nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
+    }
 
     public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 3, 0, 4, 2};
+        System.out.println(removeElement(nums, 2));
 
     }
 }
