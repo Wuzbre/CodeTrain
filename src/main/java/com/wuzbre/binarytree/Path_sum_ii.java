09f1f21d131a956706ca067c9874f50739561dad
wuzbre <15575079135@163.com>
2024-08-05 10:38:04 +0800

    msg:路径和完成，也不难，用一个减的思想判断，但是要用回溯。

---
+++ src/main/java/com/wuzbre/binarytree/Path_sum_ii.java
@@ -0,0 +1,37 @@
+package com.wuzbre.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Path_sum_ii {
+    public List<List<Integer>> pathSum(TreeNode root, int targetsum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res; // 非空判断
+
+        List<Integer> path = new LinkedList<>();
+        preorderdfs(root, targetsum, res, path);
+        return res;
+    }
+
+    public void preorderdfs(TreeNode root, int targetsum, List<List<Integer>> res, List<Integer> path) {
+        path.add(root.val);
+        // 遇到了叶子节点
+        if (root.left == null && root.right == null) {
+            // 找到了和为 targetsum 的路径
+            if (targetsum - root.val == 0) {
+                res.add(path);
+            }
+            return; // 如果和不为 targetsum，返回
+        }
+
+        if (root.left != null) {
+            preorderdfs(root.left, targetsum - root.val, res, path);
+            path.remove(path.size() - 1); // 回溯
+        }
+        if (root.right != null) {
+            preorderdfs(root.right, targetsum - root.val, res, path);
+            path.remove(path.size() - 1); // 回溯
+        }
+    }
+}
